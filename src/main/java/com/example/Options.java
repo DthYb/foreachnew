e1956a36fb8f19513175425ed9ffcab7796cb64a
DthYb <58145434+DthYb@users.noreply.github.com>
2024-12-17 10:07:33 +0100

    feat: new options

--- src/main/java/com/example/Options.java
+++ src/main/java/com/example/Options.java
@@ -3,6 +3,18 @@ package com.example;
 public class Options {
     private nom nomOptions;
 
+    private Options(nom nomOptions) {
+        this.nomOptions = nomOptions;
+    }
+
+    public nom getNomOptions() {
+        return nomOptions;
+    }
+
+    public void setNomOptions(nom nomOptions) {
+        this.nomOptions = nomOptions;
+    }
+
     enum nom {
         GPS,
         TunerFM,
